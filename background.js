74b2ae1df1864d5686358880ee1046d751bb39af
Gradey Cullins <gradeycullins@gmail.com>
2020-01-07 15:59:56 -0700

    Fix infinite redirect loop bug
    All images are now replaced by the filler image at fillerImgURL

--- background.js
+++ background.js
@@ -2,28 +2,18 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Not sure if this works.
-// chrome.storage.local.set({yikes: 'neocondon'});
-
-// chrome.webRequest.onBeforeRequest.addListener(
-//   function(details) {
-//     console.log(details.url)
-//     return {cancel: details.url.indexOf("://www.gradeycullins.com/") != -1};
-//   },
-//   {urls: ["<all_urls>"]},
-//   ["blocking"]);
+const fillerImgURL = "https://ichef.bbci.co.uk/news/410/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg"
 
 chrome.runtime.onInstalled.addListener(function() {
   console.log("Installed.");
 
-
   chrome.webRequest.onBeforeRequest.addListener(details => {
     return {
-      redirectUrl: "https://tinyurl.com/yh33bnb9"
+      redirectUrl: fillerImgURL
     }
   }, 
   {
-    urls: ["https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"],
+    urls: ["<all_urls>"],
     types: ["image"]
   }, ["blocking"] )
 });
