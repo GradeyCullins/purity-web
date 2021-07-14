c945d106e49d7789d38e84dc5fa7c5b278c88e09
Gradey Cullins <gradeycullins@gmail.com>
2021-07-14 11:53:03 -0600

    add another simple test

--- test/sum.test.js
+++ test/sum.test.js
@@ -3,3 +3,7 @@ import { sum } from '../src/content'
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3)
 })
+
+test('adds -1 + 1 to equal 0', () => {
+  expect(sum(-1, 1)).toBe(0)
+})
